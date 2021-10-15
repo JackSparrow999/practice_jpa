00aa8e3c43fe7e9d8230b0b7d6ce2fc97c035547
Ronaq <behuraronaq99@gmail.com>
2021-10-15 21:51:25 +0530

    refactor

--- src/main/java/com/revise/springboot/controllers/BooksController.java
+++ src/main/java/com/revise/springboot/controllers/BooksController.java
@@ -48,7 +48,7 @@ public class BooksController {
         return bs.getAllBooksByGenres(Genre.fromName(genre));
     }
 
-    @GetMapping("/map")
+    @GetMapping("/summary")
     public boolean mapSummaryToBook(@RequestParam("book_id") Integer bid,
                                     @RequestParam("summary_id") Integer sid){
         return bs.mapSummaryToBook(bid, sid);
